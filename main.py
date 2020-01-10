46d9332ff414bf19acc5f9d3feff225a52cf8175
kn <huyhoang8398@gmail.com>
2020-01-10 17:20:15 +0700

    init

---
+++ main.py
@@ -0,0 +1,10 @@
+import vein_detection as vd  
+import time  
+
+def main():  
+    vd_class = vd.VeinDetection(0)
+    # wait for camera to warm up
+    time.sleep(1)
+    vd_class.run()
+if __name__ == "__main__":  
+    main()
